bdd94e689380685fc16709cc8e5005a2b276d63f
Jason Alanya <jasonalanya853@gmail.com>
2022-04-26 10:57:48 -0500

    Agregado de la sección Contact

---
+++ src/Components/Skills/Skill.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Skill = () => {
+  return (
+    <div class="Skill">
+        <h2>HTML</h2>
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="100px" height="100px">
+            <defs>
+                <linearGradient id="GradientColor">
+                    <stop offset="0%" stop-color="#4961DC"></stop>
+                    <stop offset="100%" stop-color="#6A98F0"></stop>
+                </linearGradient>
+            </defs>
+            <circle cx="50" cy="50" r="40" stroke-linecap="round"></circle>
+        </svg>
+        <img src="/static/media/Html5.0f75188d2d5318f95087.png" alt="html5"/>
+    </div>
+  )
+}
+
+export default Skill
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Title from '../Title'
-
+import Skill from './Skill'
 
 const Skills = () => {
   return (
     <div id='Skills'>
       <Title texto="Skills"/>
+      <div class="Skillcontainer">
+        <Skill/>
+      </div>
     </div>
   )
 }
